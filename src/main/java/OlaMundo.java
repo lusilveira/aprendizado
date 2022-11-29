8b4df7c87c1d22740c8d698b54488798fa477039
lucianasilveir@gmail.com <lucianasilveira.ti@gmail.com>
2022-11-29 14:10:05 -0300

    Não Retorna nada

--- src/main/java/OlaMundo.java
+++ src/main/java/OlaMundo.java
@@ -1,5 +1,25 @@
 public class OlaMundo {
+
     public static void main(String[] args){
-        System.out.println("Olá Mundo!!!");
+        minhaFuncaoNaoRetornaNada();
+
+
     }
+    static void minhaFuncaoNaoRetornaNada(){
+        System.out.println("Não Retorna nada");
+    }
+    static int minhaFuncaoRetornaInteiro() {
+        System.out.println("Minha Função retorna um inteiro");
+
+
+       int numeroRetornado = 10;
+    return numeroRetornado;
+    }
+    static String minhaFuncaRetornaRecebeString(String meuParam){
+        meuParam = "Deve retornar este parametro";
+
+        System.out.println(meuParam);
+        return meuParam;
+     }
+
 }
