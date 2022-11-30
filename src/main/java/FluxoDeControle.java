5eaa34aa15857ef25e3847122e165c423a8bfd36
lucianasilveir@gmail.com <lucianasilveira.ti@gmail.com>
2022-11-30 09:50:50 -0300

    FluxoDeControle_01

---
+++ src/main/java/FluxoDeControle.java
@@ -0,0 +1,19 @@
+public class FluxoDeControle {
+
+    public static void main(String[] args){
+        boolean ehVerdade = true;
+
+        if (ehVerdade == true){
+            System.out.println("Eh verdade");
+        }
+
+        ehVerdade = false;
+
+                if(ehVerdade == true){
+                    System.out.println("Eh verdade");
+                }else{
+                    System.out.println("Não eh verdade");
+                }
+    }
+
+}
