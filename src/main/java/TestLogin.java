46d040e114942e9480dbb0088a911ea14eed7ba9
lucianasilveir@gmail.com <lucianasilveira.ti@gmail.com>
2023-01-05 11:36:37 -0300

    4.2.1 AO 4.6.1 – HANDS ON

---
+++ src/main/java/TestLogin.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class TestLogin {
+    public static void main(String[] args){
+        System.setProperty("webdriver.chrome.driver", "C:\\ChromeDriver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.navigate().to("http://www.blazedemo.com/login");
+
+        WebElement textoEmail = driver.findElement(By.id("email"));
+        textoEmail.sendKeys("lucianasilveir@gmail.com" );
+        WebElement textoSenha = driver.findElement(By.id("passowird"));
+        textoSenha.sendKeys("NSbnpGccSNtjB2G");
+        driver.findElement(By.tagName("button"));
+        List<WebElement> listaBotoes = driver.findElements(By.tagName("button"));
+
+        for(WebElement botao : listaBotoes) {
+            if (botao.getAttribute("type").equals("sumit")) {
+                botao.submit();
+            }
+
+        }
+    }
+}
